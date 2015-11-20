978f321fa29bc6b11149c1524d4f0dc1197c53c2
unknown <aashislamsal@AFSPCHPA23810BJ.usda.net>
2015-11-20 16:10:47 -0700

    Refactoring

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,218 +1,242 @@
 (function(){
     
-    //Date format as of input CSV
-    var format = d3.time.format("%Y-%m-%d");
-    
-    //Append groomed variables on main object  
-    function parser(d) {        
-        d.pValue = +d.Value;
-        d.pYearOnly = +d.Year;
-        d.pDate = format.parse(d.Date);       
-        return d; 
-    }    
-   
-    //Create formatted X-axis labels
-    var generateXAxisLables = function(){
-        var formattedLabel = [];    
-        for(var day=1;day<366;++day){
-            var momentDate = Date.fromDayOfYear(day);
-            var label = pad(momentDate.getMonth() + 1)+"-"+pad(momentDate.getDate());
-            formattedLabel.push(label);    
-        }
-        return formattedLabel;
+    var plotOptions = {
+        csvPath:'assets/all_years_formatted2.csv',
+        width:700,
+        height:400,
+        plotXaxisDescription:"Actual Values ts",
+        plotYaxixDescription:"Percen tt",
+        plotTitleDescription:"Nutters Ridge - Juniper, Utah ta",
+        plotYaxisRangeMin:40,
+        plotYaxixRangeMax:150
+                   
     };
     
-    //Plot main chart
-    function plotMultiLineChart(chartData) {   
-            
-        var margin = {top: 30, right: 30, bottom: 75, left: 100};
-        var width = 700 - margin.left - margin.right;
-        var height = 400 - margin.top - margin.bottom;
-        
-        //xAxis interval
-        var minDay = 1;
-        var maxDaY = 365;
-        
-        //yAxix interval
-        var yAxisMaxValue = 130;
-        var yAxisMinValue = 40;
-        
-        //Axis ticks
-        var xAxisTickNumber = 20;
-        var yAxisTickNumber = 7;
-        
-        var x = d3.scale.linear()
-            .domain([minDay,maxDaY]).nice()
-            .range([0, width]);
     
-        var y = d3.scale.linear()
-            .domain([yAxisMinValue,yAxisMaxValue])
-            .range([height, 0]);    
-        
-        var xAxisLabels = generateXAxisLables();
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .ticks(xAxisTickNumber)
-            .tickFormat(function(d){  
-                 return(xAxisLabels[d]);            
-               })
-            .orient("bottom");
     
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .ticks(yAxisTickNumber)
-            .orient("left");
+    
+    var myval = new PlotMultiYearActualData(plotOptions);
+    myval.plotChart();
+    
+    function PlotMultiYearActualData(plotOptions){
         
-        //Group data sets by year
-        var dataGroupByYear = d3.nest()
-            .key(function(d){return d.pYearOnly;})
-            .entries(chartData);
+        this.plotChart = plotInteractiveChart;
+
+        
+         function plotInteractiveChart (){
+             
+            //Date format as of input CSV
+            var format = d3.time.format("%Y-%m-%d");
             
-        // put the graph in the "miles" div
-        var svg = d3.select("#graph").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            //Append groomed variables on main object  
+            function parser(d) {        
+                d.pValue = +d.Value;
+                d.pYearOnly = +d.Year;
+                d.pDate = format.parse(d.Date);       
+                return d; 
+            }    
         
-        //Mouseover tip
-        var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([70, 40])
-            .html(function(d) {
-                return "Date: "+ d.pDate.getFullYear() +"-"+ (parseInt(d.pDate.getMonth())+1) +"-"+ d.pDate.getDate() +                   
-                        "<br/>"+ "Value: "+ d.pValue +" %" ;
+            //Create formatted X-axis labels
+            var generateXAxisLables = function(){
+                var formattedLabel = [];    
+                for(var day=1;day<365;++day){
+                    var momentDate = Date.fromDayOfYear(day);
+                    var label = pad(momentDate.getMonth() + 1)+"-"+pad(momentDate.getDate());
+                    formattedLabel.push(label);    
+                }
+                return formattedLabel;
+            };
+            
+            //Plot main chart
+            function plotMultiLineChart(chartData) {   
+                    
+                var margin = {top: 30, right: 30, bottom: 75, left: 100};
+                                
+                //xAxis interval
+                var minDay = 1;
+                var maxDaY = 365;
+                
+                //yAxix interval
+                var yAxisMaxValue = 130;
+                var yAxisMinValue = 40;
+                
+                //Axis ticks
+                var xAxisTickNumber = 20;
+                var yAxisTickNumber = 7;
+                
+                var x = d3.scale.linear()
+                    .domain([minDay,maxDaY]).nice()
+                    .range([0, plotOptions.width]);
+            
+                var y = d3.scale.linear()
+                    .domain([yAxisMinValue,yAxisMaxValue])
+                    .range([plotOptions.height, 0]);    
+                
+                var xAxisLabels = generateXAxisLables();
+                var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .ticks(xAxisTickNumber)
+                    .tickFormat(function(d){  
+                        return(xAxisLabels[d]);            
+                    })
+                    .orient("bottom");
+            
+                var yAxis = d3.svg.axis()
+                    .scale(y)
+                    .ticks(yAxisTickNumber)
+                    .orient("left");
+                
+                //Group data sets by year
+                var dataGroupByYear = d3.nest()
+                    .key(function(d){return d.pYearOnly;})
+                    .entries(chartData);
+                    
+                // put the graph in the "miles" div
+                var svg = d3.select("#graph").append("svg")
+                    .attr("width", plotOptions.width + margin.left + margin.right)
+                    .attr("height", plotOptions.height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                
+                //Mouseover tip
+                var tip = d3.tip()
+                    .attr('class', 'd3-tip')
+                    .offset([70, 40])
+                    .html(function(d) {
+                        return "Date: "+ d.pDate.getFullYear() +"-"+ (parseInt(d.pDate.getMonth())+1) +"-"+ d.pDate.getDate() +                   
+                                "<br/>"+ "Value: "+ d.pValue +" %" ;
+                        });
+            
+                svg.call(tip);       
+                
+                var line = d3.svg.line()
+                    .x(function(d) {
+                        return x(d.pDate.dayOfYear());
+                    } )
+                    .y(function(d) { return y(d.pValue); } ); 
+                
+                dataGroupByYear.forEach(function(d,i){
+                    svg.append("path")
+                        .attr("class", "line")
+                        .style("stroke", function() { 
+                            // Add the colours dynamically
+                            d.color = color(d.key);
+                            return d.color; })
+                        
+                        .attr("id", 'line_'+d.key.replace(/\s+/g, '')) // assign ID
+                        .attr("d",line(d.values));
+                    
+                    //Add legend
+                    var lWidth = plotOptions.width-30;
+                    var lSpace = 20;
+                    svg.append("text")
+                        .attr("x", lWidth)
+                        .attr("y", (lSpace / 2) + i * lSpace)
+                        .style("fill", function(){
+                            return color(d.key);
+                        })
+                        .attr("class", "legend")
+                        .text(d.key)
+                        .on('click',function(){
+                            //Toggle chart
+                        var active = d.active ? false : true;
+                        var opacity = active ? 0 : 1;                
+                        d3.select("#line_" + d.key).style("opacity", opacity); 
+                        d3.selectAll(".circle_" + d.key).style("opacity", opacity);               
+                        d.active = active;
+                        })
+                        
+                        .on("mouseover", function () {
+                            d3.select("#line_" + d.key).style("stroke-width", 8);
+                            d3.selectAll(".circle_" + d.key).style("stroke-width", 8);
+                        })
+                        .on("mouseout",function(){
+                            d3.select("#line_" + d.key).style("stroke-width", 4);
+                            d3.selectAll(".circle_" + d.key).style("stroke-width", 4);
+                        });               
+                        
                 });
-    
-        svg.call(tip);       
-
-        
-        var line = d3.svg.line()
-            .x(function(d) {
-                return x(d.pDate.dayOfYear());
-            } )
-            .y(function(d) { return y(d.pValue); } ); 
-           
-        dataGroupByYear.forEach(function(d,i){
-            svg.append("path")
-                .attr("class", "line")
-                .style("stroke", function() { 
-                    // Add the colours dynamically
-                    d.color = color(d.key);
-                    return d.color; })
                 
-                .attr("id", 'line_'+d.key.replace(/\s+/g, '')) // assign ID
-                .attr("d",line(d.values));
-            
-            //Add legend
-            var lWidth = width-30;
-            var lSpace = 20;
-            svg.append("text")
-                .attr("x", lWidth)
-                .attr("y", (lSpace / 2) + i * lSpace)
-                .style("fill", function(){
-                    return color(d.key);
-                })
-                .attr("class", "legend")
-                .text(d.key)
-                .on('click',function(){
-                    //Toggle chart
-                   var active = d.active ? false : true;
-                   var opacity = active ? 0 : 1;                
-                   d3.select("#line_" + d.key).style("opacity", opacity); 
-                   d3.selectAll(".circle_" + d.key).style("opacity", opacity);               
-                   d.active = active;
-                })
+                // Add the X Axis
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + plotOptions.height + ")")
+                    .call(xAxis.tickSize(-plotOptions.height, 0, 0))
+                    .selectAll("text")  
+                        .style("text-anchor", "end")
+                        .attr("dx", "-.5em")
+                        .attr("dy", ".15em")
+                        .attr("transform", "rotate(-55)" );
+                    
+                svg.append("text")
+                    .attr("class", "xlabel")
+                    .attr("text-anchor", "middle")
+                    .attr("x", plotOptions.width / 2)
+                    .attr("y", plotOptions.height + margin.bottom)
+                    .text(plotOptions.plotXaxisDescription);
                 
-                .on("mouseover", function () {
-                    d3.select("#line_" + d.key).style("stroke-width", 8);
-                    d3.selectAll(".circle_" + d.key).style("stroke-width", 8);
-                })
-                .on("mouseout",function(){
-                    d3.select("#line_" + d.key).style("stroke-width", 4);
-                    d3.selectAll(".circle_" + d.key).style("stroke-width", 4);
-                });               
+                // Add the Y Axis
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis.tickSize(-plotOptions.width, 0, 0));  
+                    
+                svg.append("text")
+                    .attr("class", "ylabel")
+                    .attr("y", 30 - margin.left) // x and y switched due to rotation!!
+                    .attr("x", 0 - (plotOptions.height / 2))
+                    .attr("dy", "1em")
+                    .attr("transform", "rotate(-90)")
+                    .style("text-anchor", "middle")
+                    .text(plotOptions.plotYaxixDescription);
                 
-        });
-        
-        // Add the X Axis
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis.tickSize(-height, 0, 0))
-            .selectAll("text")  
-                .style("text-anchor", "end")
-                .attr("dx", "-.5em")
-                .attr("dy", ".15em")
-                .attr("transform", "rotate(-55)" );
+                //Show chart circles
+                svg.selectAll(".dot")
+                    .data(chartData)
+                    .enter().append("circle")
+                    .attr('class',function(d){
+                        return 'datapoint circle_'+d.pYearOnly;
+                    })
+                    .attr('cx', function(d) { return x(d.pDate.dayOfYear()); })
+                    .attr('cy', function(d) { return y(d.pValue); })
+                    .attr('r', 3)
+                    .attr('fill', 'black')
+                    .attr('stroke', function(d){
+                        // Add the colours dynamically
+                        d.color = color(d.pYearOnly);
+                        return d.color;
+                    })
+                    .attr('stroke-width', '3')
+                    .on('mouseover', tip.show)
+                    .on('mouseout', tip.hide);
+                    
+                //Graph title
+                svg.append("text")
+                    .attr("class", "graphtitle")
+                    .attr("y", 0)
+                    .attr("x", plotOptions.width/2)
+                    .style("text-anchor", "middle")
+                    .text(plotOptions.plotTitleDescription);            
+            }
             
-        svg.append("text")
-            .attr("class", "xlabel")
-            .attr("text-anchor", "middle")
-            .attr("x", width / 2)
-            .attr("y", height + margin.bottom)
-            .text("Actual values");
-        
-        // Add the Y Axis
-        svg.append("g")
-            .attr("class", "y axis")
-            //.style("stroke-dasharray", (5, 5))
-            .call(yAxis.tickSize(-width, 0, 0));  
+            // set the colour scale
+            var color = d3.scale.ordinal()
+                        .range(colorbrewer.Set1[9]);  
             
-        svg.append("text")
-            .attr("class", "ylabel")
-            .attr("y", 30 - margin.left) // x and y switched due to rotation!!
-            .attr("x", 0 - (height / 2))
-            .attr("dy", "1em")
-            .attr("transform", "rotate(-90)")
-            .style("text-anchor", "middle")
-            .text("Percent");
-        
-        //Show chart circles
-        svg.selectAll(".dot")
-            .data(chartData)
-            .enter().append("circle")
-            .attr('class',function(d){
-                return 'datapoint circle_'+d.pYearOnly;
-            })
-            .attr('cx', function(d) { return x(d.pDate.dayOfYear()); })
-            .attr('cy', function(d) { return y(d.pValue); })
-            .attr('r', 3)
-            .attr('fill', 'black')
-            .attr('stroke', function(d){
-                // Add the colours dynamically
-                d.color = color(d.pYearOnly);
-                return d.color;
-            })
-            .attr('stroke-width', '3')
-            .on('mouseover', tip.show)
-            .on('mouseout', tip.hide);
+            // Read in .csv data and make graph
+            d3.csv(plotOptions.csvPath, parser,
+                function(error, csvData) {     
             
-        //Graph title
-        svg.append("text")
-            .attr("class", "graphtitle")
-            .attr("y", 0)
-            .attr("x", width/2)
-            .style("text-anchor", "middle")
-            .text("Nutters Ridge - Juniper, Utah");            
-     }
+                //Remove NA entries    
+                var cleanData = csvData.filter(function(row){
+                if(row.Value !='NA'){
+                    return row.Value;
+                }
+                return false;
+                });
+            plotMultiLineChart(cleanData);
+            }); 
+        }
     
-    // set the colour scale
-    var color = d3.scale.ordinal()
-                .range(colorbrewer.Set1[9]);  
-    
-    // Read in .csv data and make graph
-    d3.csv("assets/all_years_formatted2.csv", parser,
-        function(error, csvData) {     
-    
-        //Remove NA entries    
-        var cleanData = csvData.filter(function(row){
-           if(row.Value !='NA'){
-               return row.Value;
-           }
-           return false;
-        });
-       plotMultiLineChart(cleanData);
-    }); 
-
+        
+    }
 })();
